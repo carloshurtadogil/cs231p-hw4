7d1fd460b969b1144aa557889b1b8782793612de
Carlos Hurtaado <carlos.hurtado19@yahoo.com>
2020-11-03 22:07:59 -0800

    mat_multiply single processor

--- multiply.c
+++ multiply.c
@@ -9,7 +9,7 @@ typedef struct { //example structure
 
 double dot_product(Mat*, Mat*, int);
 
-static void example_helper_function(int n){
+/*static void example_helper_function(int n){
     // Functions defined with the modifier 'static' are only visible
     // to other functions in this file. They cannot be called from
     // outside (for example, from main.c). Use them to organize your
@@ -29,31 +29,27 @@ static void example_helper_function(int n){
     es1.e_g = 7;
     printf("n = %d\n", es1.example + es1.e_g + n);
     return;
-}
+}*/
 
 void mat_multiply(Mat *A, Mat *B, Mat *C, unsigned int threads){
     //Put your code here.
     //Remember to set the correct values for C->m, and C->n
     //after doing the multiplication.
     //example_helper_function(1000);
-    return;
-}
-
-/**
- ** Calculate and return the dot product of two pointer arrays
- ** 
- ** @param X Pointer array
- ** @param Y Pointer array
- ** @param n Size of both arrays
- ** @return The dot product of X and Y
- **/
-double dot_product(Mat *X, Mat *Y, int n) {
-    double product = 0.0;
-
-    // product = product + X[i] + Y[i]
+    int n = A->n;
+    int product;
     for (int i = 0; i < n; i++) {
-        product = product + *(X->ptr + i) * *(Y->ptr + i);
+        //printf("i = %d\n", i);
+        for (int j = 0; j < n; j++) {
+            //printf("j = %d: ", j);
+            for (int k = 0; k < n; k++) {
+                product = A->ptr[i*n + k] * B->ptr[k*n + j];
+                C->ptr[i*n + j] = C->ptr[i*n + j] + product;
+                //printf("%d ", (i*n + j));
+            }
+            //printf("\n");
+        }
+        //printf("\n");
     }
-
-    return product;
+    return;
 }
